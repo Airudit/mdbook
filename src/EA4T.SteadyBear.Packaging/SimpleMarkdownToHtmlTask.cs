c2bc86732fa4087ec8ffbf7f9b8082253b867dc6
Antoine Sottiau <antoine@ea4t.com>
2020-10-20 11:29:48 +0200

    MarkdownToHtmlTask: fixes (thx to Thibaut)

--- src/EA4T.SteadyBear.Packaging/SimpleMarkdownToHtmlTask.cs
+++ src/EA4T.SteadyBear.Packaging/SimpleMarkdownToHtmlTask.cs
@@ -148,7 +148,7 @@ namespace EA4T.SteadyBear.Packager
                 }
                 else if ("Info".Equals(key, StringComparison.Ordinal))
                 {
-                    return WebUtility.HtmlEncode(string.Format(CultureInfo.InvariantCulture, "This document was generated from file \"{0}\" on {1:o} using the MarkdownToHtmlTask. Manual modifications will be lost next time this file is generated again. ", fileName, DateTime.UtcNow));
+                    return WebUtility.HtmlEncode(string.Format(CultureInfo.InvariantCulture, "This document was generated automatically from file \"{0}\" on {1:o} using the MarkdownToHtmlTask tool. Manual modifications will be lost next time this file is generated again. ", fileName, DateTime.UtcNow));
                 }
                 else
                 {
